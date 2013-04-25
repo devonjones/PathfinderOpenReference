2f90e5001f459dbcd4655d647ff3aaa8884adf3a
Devon Jones <devon.jones@gmail.com>
2013-04-25 17:51:13 -0600

    updating item renderer to display new item fields

--- src/org/evilsoft/pathfinder/reference/db/book/ItemAdapter.java
+++ src/org/evilsoft/pathfinder/reference/db/book/ItemAdapter.java
@@ -33,29 +33,63 @@ public class ItemAdapter {
 		public static String getAura(Cursor cursor) {
 			return cursor.getString(0);
 		}
+
 		public static String getSlot(Cursor cursor) {
 			return cursor.getString(1);
 		}
+
 		public static String getCl(Cursor cursor) {
 			return cursor.getString(2);
 		}
+
 		public static String getPrice(Cursor cursor) {
 			return cursor.getString(3);
 		}
+
 		public static String getWeight(Cursor cursor) {
 			return cursor.getString(4);
 		}
+
 		public static String getRequirements(Cursor cursor) {
 			return cursor.getString(5);
 		}
+
 		public static String getSkill(Cursor cursor) {
 			return cursor.getString(6);
 		}
+
 		public static String getCrIncrease(Cursor cursor) {
 			return cursor.getString(7);
 		}
+
 		public static String getCost(Cursor cursor) {
 			return cursor.getString(8);
+		}
+	}
+
+	public Cursor getItemMisc(Integer sectionId) {
+		List<String> args = new ArrayList<String>();
+		args.add(sectionId.toString());
+		StringBuffer sb = new StringBuffer();
+		sb.append("SELECT field, subsection, value");
+		sb.append(" FROM item_misc");
+		sb.append(" WHERE section_id = ?");
+		sb.append(" ORDER BY subsection");
+		String sql = sb.toString();
+		return database.rawQuery(sql, BaseDbHelper.toStringArray(args));
+	}
+
+	public static class ItemMiscUtils {
+		public static String getField(Cursor cursor) {
+			return cursor.getString(0);
+		}
+
+		public static String getSubsection(Cursor cursor) {
+			return cursor.getString(1);
+		}
+
+		public static String getValue(Cursor cursor) {
+			return cursor.getString(2);
 		}
 	}
 }
--- src/org/evilsoft/pathfinder/reference/render/ItemRenderer.java
+++ src/org/evilsoft/pathfinder/reference/render/ItemRenderer.java
@@ -19,25 +19,63 @@ public class ItemRenderer extends StatBlockRenderer {
 
 	@Override
 	public String renderDetails() {
-		Cursor cursor = bookDbAdapter.getItemAdapter().getItemDetails(sectionId);
+		StringBuffer sb = new StringBuffer();
+		sb.append(renderItemDetails());
+		sb.append(renderItemMisc());
+		return sb.toString();
+	}
+
+	public String renderItemDetails() {
+		Cursor cursor = bookDbAdapter.getItemAdapter()
+				.getItemDetails(sectionId);
 		try {
 			StringBuffer sb = new StringBuffer();
 			boolean has_next = cursor.moveToFirst();
 			if (has_next) {
-				sb.append(addField("Aura", ItemAdapter.ItemUtils.getAura(cursor), false));
+				sb.append(addField("Aura",
+						ItemAdapter.ItemUtils.getAura(cursor), false));
 				sb.append(addField("CL", ItemAdapter.ItemUtils.getCl(cursor)));
-				sb.append(addField("Slot", ItemAdapter.ItemUtils.getSlot(cursor), false));
-				sb.append(addField("Price", ItemAdapter.ItemUtils.getPrice(cursor), false));
-				sb.append(addField("Weight", ItemAdapter.ItemUtils.getWeight(cursor)));
+				sb.append(addField("Slot",
+						ItemAdapter.ItemUtils.getSlot(cursor), false));
+				sb.append(addField("Price",
+						ItemAdapter.ItemUtils.getPrice(cursor), false));
+				sb.append(addField("Weight",
+						ItemAdapter.ItemUtils.getWeight(cursor)));
 				// TODO: Construction and Description reversed due to child
 				// rendering
 				sb.append(renderStatBlockBreaker("Construction"));
-				sb.append(addField("Requirements", ItemAdapter.ItemUtils.getRequirements(cursor), false));
-				sb.append(addField("Skill", ItemAdapter.ItemUtils.getSkill(cursor), false));
-				sb.append(addField("CR Increase", ItemAdapter.ItemUtils.getCrIncrease(cursor), false));
-				sb.append(addField("Cost", ItemAdapter.ItemUtils.getCost(cursor)));
+				sb.append(addField("Requirements",
+						ItemAdapter.ItemUtils.getRequirements(cursor), false));
+				sb.append(addField("Skill",
+						ItemAdapter.ItemUtils.getSkill(cursor), false));
+				sb.append(addField("CR Increase",
+						ItemAdapter.ItemUtils.getCrIncrease(cursor), false));
+				sb.append(addField("Cost",
+						ItemAdapter.ItemUtils.getCost(cursor)));
 				sb.append(renderStatBlockBreaker("Description"));
 				this.suppressNextTitle = true;
+			}
+			return sb.toString();
+		} finally {
+			cursor.close();
+		}
+	}
+
+	public String renderItemMisc() {
+		Cursor cursor = bookDbAdapter.getItemAdapter().getItemMisc(sectionId);
+		try {
+			StringBuffer sb = new StringBuffer();
+			boolean has_next = cursor.moveToFirst();
+			String lastSection = "";
+			if (has_next) {
+				String currentSection = ItemAdapter.ItemMiscUtils
+						.getSubsection(cursor);
+				if (!lastSection.equals(currentSection)) {
+					sb.append(renderStatBlockBreaker("currentSection"));
+					lastSection = currentSection;
+				}
+				sb.append(addField(ItemAdapter.ItemMiscUtils.getField(cursor),
+						ItemAdapter.ItemMiscUtils.getValue(cursor), false));
 			}
 			return sb.toString();
 		} finally {
