97149b3c8c1cf7be59810faf083037653237f994
Devon Jones <devon.jones@gmail.com>
2013-05-04 07:43:02 -0600

    culling dead code

--- src/org/evilsoft/pathfinder/reference/DetailsListFragment.java
+++ src/org/evilsoft/pathfinder/reference/DetailsListFragment.java
@@ -16,9 +16,10 @@ public class DetailsListFragment extends AbstractViewListFragment {
 		super();
 		super.thin = true;
 	}
-	
+
 	@Override
-	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+	public void onItemClick(AdapterView<?> parent, View view, int position,
+			long id) {
 		StringBuffer sb = new StringBuffer();
 		sb.append("DetailsListFragment.onItemClick: position:");
 		sb.append(position);
@@ -26,69 +27,26 @@ public class DetailsListFragment extends AbstractViewListFragment {
 		sb.append(id);
 		ErrorReporter e = ErrorReporter.getInstance();
 		e.putCustomData("LastClick", sb.toString());
-		if(empty) {
+		if (empty) {
 			return;
 		}
 		String uri = getNextUrl(position);
 		Log.d(TAG, uri);
-		if(PathfinderOpenReferenceActivity.isTabletLayout(getActivity())) {
-			DetailsViewFragment viewer = (DetailsViewFragment) this.getActivity().getSupportFragmentManager()
-				.findFragmentById(R.id.details_view_fragment);
+		if (PathfinderOpenReferenceActivity.isTabletLayout(getActivity())) {
+			DetailsViewFragment viewer = (DetailsViewFragment) this
+					.getActivity().getSupportFragmentManager()
+					.findFragmentById(R.id.details_view_fragment);
 			viewer.updateUrl(uri, currentUrl);
 		} else {
-			Intent showContent = new Intent(this.getActivity().getApplicationContext(), DetailsActivity.class);
-
+			Intent showContent = new Intent(this.getActivity()
+					.getApplicationContext(), DetailsActivity.class);
 			showContent.setData(Uri.parse(uri));
 			startActivity(showContent);
 		}
 	}
 
 	private String getNextUrl(Integer position) {
-		BaseListItem item = (BaseListItem)currentListAdapter.getItem(position);
+		BaseListItem item = (BaseListItem) currentListAdapter.getItem(position);
 		return item.getUrl();
 	}
-
-	
-	/*public void updateUrl(String newUrl) {
-		currentType = null;
-		currentUrl = newUrl;
-		Log.i(TAG, newUrl);
-		ErrorReporter e = ErrorReporter.getInstance();
-		e.putCustomData("LastDetailsListUrl", newUrl);
-		this.getListView().setOnItemClickListener(this);
-		this.getListView().setCacheColorHint(Color.WHITE);
-		if (newUrl == null || checkUrlEqual(newUrl)) {
-			return;
-		}
-		
-		Cursor cursor = dbWrangler.getIndexDbAdapter().getIndexGroupAdapter().fetchByUrl(currentUrl);
-		try {
-			String[] parts = newUrl.split("\\/");
-			boolean has_item = cursor.moveToFirst();
-			if (has_item) {
-				String name = IndexGroupAdapter.IndexGroupUtils.getName(cursor);
-				String type = IndexGroupAdapter.IndexGroupUtils.getType(cursor);
-				getListAdapter(name, type, subtype);
-			}
-			else if (parts[2].equals("Search")) {
-				if (parts.length == 4) {
-					Cursor searchcurs = dbWrangler.getIndexDbAdapter().getSearchAdapter().search(parts[3]);
-					cursorList.add(searchcurs);
-					currentListAdapter = new SearchListAdapter(getActivity().getApplicationContext(), searchcurs);
-					if (currentListAdapter.isEmpty()) {
-						empty = true;
-						ArrayList<String> list = new ArrayList<String>();
-						list.add("(No Results)");
-						currentListAdapter = new ArrayAdapter<String>(getActivity().getApplicationContext(), R.layout.list_item,
-							list);
-					}
-				}
-			} else {
-				empty = false;
-			}
-			setListAdapter(currentListAdapter);
-		} finally {
-			cursor.close();
-		}
-	}*/
 }
