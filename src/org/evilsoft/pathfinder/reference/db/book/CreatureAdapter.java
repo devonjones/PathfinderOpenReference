6752299e56cb9c32314bd1c131f852a79cc38a1d
Devon Jones <devon.jones@gmail.com>
2013-05-11 15:17:45 -0600

    Fixing creature rendering, specifically Natural Armor and Hit Dice

--- src/org/evilsoft/pathfinder/reference/db/book/CreatureAdapter.java
+++ src/org/evilsoft/pathfinder/reference/db/book/CreatureAdapter.java
@@ -36,6 +36,220 @@ public class CreatureAdapter {
 		return database.rawQuery(sql, BaseDbHelper.toStringArray(args));
 	}
 
+	public static class CreatureUtils {
+		public static String getSex(Cursor cursor) {
+			return cursor.getString(0);
+		}
+
+		public static String getSuperRace(Cursor cursor) {
+			return cursor.getString(1);
+		}
+
+		public static String getLevel(Cursor cursor) {
+			return cursor.getString(2);
+		}
+
+		public static String getCR(Cursor cursor) {
+			return cursor.getString(3);
+		}
+
+		public static String getXP(Cursor cursor) {
+			return cursor.getString(4);
+		}
+
+		public static String getAlignment(Cursor cursor) {
+			return cursor.getString(5);
+		}
+
+		public static String getSize(Cursor cursor) {
+			return cursor.getString(6);
+		}
+
+		public static String getCreatureType(Cursor cursor) {
+			return cursor.getString(7);
+		}
+
+		public static String getCreatureSubtype(Cursor cursor) {
+			return cursor.getString(8);
+		}
+
+		public static String getInit(Cursor cursor) {
+			return cursor.getString(9);
+		}
+
+		public static String getSenses(Cursor cursor) {
+			return cursor.getString(10);
+		}
+
+		public static String getAura(Cursor cursor) {
+			return cursor.getString(11);
+		}
+
+		public static String getAC(Cursor cursor) {
+			return cursor.getString(12);
+		}
+
+		public static String getHP(Cursor cursor) {
+			return cursor.getString(13);
+		}
+
+		public static String getFortitude(Cursor cursor) {
+			return cursor.getString(14);
+		}
+
+		public static String getReflex(Cursor cursor) {
+			return cursor.getString(15);
+		}
+
+		public static String getWill(Cursor cursor) {
+			return cursor.getString(16);
+		}
+
+		public static String getDefensiveAbilities(Cursor cursor) {
+			return cursor.getString(17);
+		}
+
+		public static String getDR(Cursor cursor) {
+			return cursor.getString(18);
+		}
+
+		public static String getResist(Cursor cursor) {
+			return cursor.getString(19);
+		}
+
+		public static String getImmune(Cursor cursor) {
+			return cursor.getString(20);
+		}
+
+		public static String getSR(Cursor cursor) {
+			return cursor.getString(21);
+		}
+
+		public static String getWeaknesses(Cursor cursor) {
+			return cursor.getString(22);
+		}
+
+		public static String getSpeed(Cursor cursor) {
+			return cursor.getString(23);
+		}
+
+		public static String getMelee(Cursor cursor) {
+			return cursor.getString(24);
+		}
+
+		public static String getRanged(Cursor cursor) {
+			return cursor.getString(25);
+		}
+
+		public static String getSpace(Cursor cursor) {
+			return cursor.getString(26);
+		}
+
+		public static String getReach(Cursor cursor) {
+			return cursor.getString(27);
+		}
+
+		public static String getSpecialAttacks(Cursor cursor) {
+			return cursor.getString(28);
+		}
+
+		public static String getStrength(Cursor cursor) {
+			return cursor.getString(29);
+		}
+
+		public static String getDexterity(Cursor cursor) {
+			return cursor.getString(30);
+		}
+
+		public static String getConstitution(Cursor cursor) {
+			return cursor.getString(31);
+		}
+
+		public static String getIntelligence(Cursor cursor) {
+			return cursor.getString(32);
+		}
+
+		public static String getWisdom(Cursor cursor) {
+			return cursor.getString(33);
+		}
+
+		public static String getCharisma(Cursor cursor) {
+			return cursor.getString(34);
+		}
+
+		public static String getBaseAttack(Cursor cursor) {
+			return cursor.getString(35);
+		}
+
+		public static String getCMB(Cursor cursor) {
+			return cursor.getString(36);
+		}
+
+		public static String getCMD(Cursor cursor) {
+			return cursor.getString(37);
+		}
+
+		public static String getFeats(Cursor cursor) {
+			return cursor.getString(38);
+		}
+
+		public static String getSkills(Cursor cursor) {
+			return cursor.getString(39);
+		}
+
+		public static String getRacialModifiers(Cursor cursor) {
+			return cursor.getString(40);
+		}
+
+		public static String getLanguages(Cursor cursor) {
+			return cursor.getString(41);
+		}
+
+		public static String getSpecialQualities(Cursor cursor) {
+			return cursor.getString(42);
+		}
+
+		public static String getGear(Cursor cursor) {
+			return cursor.getString(43);
+		}
+
+		public static String getCombatGear(Cursor cursor) {
+			return cursor.getString(44);
+		}
+
+		public static String getOtherGear(Cursor cursor) {
+			return cursor.getString(45);
+		}
+
+		public static String getBoon(Cursor cursor) {
+			return cursor.getString(46);
+		}
+
+		public static String getEnvironment(Cursor cursor) {
+			return cursor.getString(47);
+		}
+
+		public static String getOrganization(Cursor cursor) {
+			return cursor.getString(48);
+		}
+
+		public static String getTreasure(Cursor cursor) {
+			return cursor.getString(49);
+		}
+
+		public static String getHitDice(Cursor cursor) {
+			return cursor.getString(50);
+		}
+
+		public static String getNaturalArmor(Cursor cursor) {
+			return cursor.getString(51);
+		}
+
+		public static String getBreathWeapon(Cursor cursor) {
+			return cursor.getString(52);
+		}
+	}
+
 	public Cursor getCreatureSpells(Integer sectionId) {
 		List<String> args = new ArrayList<String>();
 		args.add(sectionId.toString());
@@ -45,5 +259,15 @@ public class CreatureAdapter {
 		sb.append(" WHERE section_id = ?");
 		String sql = sb.toString();
 		return database.rawQuery(sql, BaseDbHelper.toStringArray(args));
+	}
+
+	public static class CreatureSpellsUtils {
+		public static String getName(Cursor cursor) {
+			return cursor.getString(0);
+		}
+
+		public static String getBody(Cursor cursor) {
+			return cursor.getString(1);
+		}
 	}
 }
--- src/org/evilsoft/pathfinder/reference/render/CreatureRenderer.java
+++ src/org/evilsoft/pathfinder/reference/render/CreatureRenderer.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.evilsoft.pathfinder.reference.db.book.BookDbAdapter;
+import org.evilsoft.pathfinder.reference.db.book.CreatureAdapter;
 
 import android.database.Cursor;
 
@@ -58,12 +59,9 @@ public class CreatureRenderer extends StatBlockRenderer {
 		return "";
 	}
 
-	private String renderCreatureHeader(Cursor curs, String desc,
+	private String renderCreatureHeader(Cursor cursor, String desc,
 			String source, String newUri, boolean top) {
-		// 0:sex, 1:super_race, 2:level, 3:cr, 4:xp, 5:alignment, 6:size,
-		// 7:creature_type, 8:creature_subtype, 9:init, 10:senses, 11:aura,
 		StringBuffer sb = new StringBuffer();
-		String cr = curs.getString(3);
 		if (desc != null) {
 			sb.append("<p>");
 			sb.append(desc);
@@ -71,57 +69,69 @@ public class CreatureRenderer extends StatBlockRenderer {
 		}
 		if (top) {
 			sb.append("<b>CR ");
-			sb.append(cr);
+			sb.append(CreatureAdapter.CreatureUtils.getCR(cursor));
 			sb.append("</b><br>\n");
 		}
-		String xp = curs.getString(4);
+		String xp = CreatureAdapter.CreatureUtils.getXP(cursor);
 		if (xp != null) {
 			sb.append("<b>XP ");
 			sb.append(xp);
 			sb.append("</b><br>\n");
 		}
 		List<String> elems = new ArrayList<String>();
-		elems.add(curs.getString(0));
-		elems.add(curs.getString(1));
-		elems.add(curs.getString(2));
+		elems.add(CreatureAdapter.CreatureUtils.getSex(cursor));
+		elems.add(CreatureAdapter.CreatureUtils.getSuperRace(cursor));
+		elems.add(CreatureAdapter.CreatureUtils.getLevel(cursor));
 		sb.append(displayLine(elems));
 		elems.clear();
-		elems.add(curs.getString(5));
-		elems.add(curs.getString(6));
-		elems.add(curs.getString(7));
-		String subtype = curs.getString(8);
+		elems.add(CreatureAdapter.CreatureUtils.getAlignment(cursor));
+		elems.add(CreatureAdapter.CreatureUtils.getSize(cursor));
+		elems.add(CreatureAdapter.CreatureUtils.getCreatureType(cursor));
+		String subtype = CreatureAdapter.CreatureUtils
+				.getCreatureSubtype(cursor);
 		if (subtype != null) {
 			elems.add("(" + subtype + ")");
 		}
 		sb.append(displayLine(elems));
-		sb.append(addField("Init", curs.getString(9), false));
-		sb.append(addField("Senses", curs.getString(10)));
-		sb.append(addField("Aura", curs.getString(11)));
+		sb.append(addField("Init",
+				CreatureAdapter.CreatureUtils.getInit(cursor), false));
+		sb.append(addField("Senses",
+				CreatureAdapter.CreatureUtils.getSenses(cursor)));
+		sb.append(addField("Aura",
+				CreatureAdapter.CreatureUtils.getAura(cursor)));
 		sb.append(addField("Source", source));
 		return sb.toString();
 	}
 
-	private String renderCreatureDefense(Cursor curs) {
+	private String renderCreatureDefense(Cursor cursor) {
 		StringBuffer sb = new StringBuffer();
-		// 12:ac, 13:hp, 14:fortitude, 15:reflex, 16:will,
-		// 17:defensive_abilities, 18:dr, 19:resist, 20:immune,
-		// 21:sr, 22:weakness,
-		// 47:hit_dice, 48:natural_armor
 		sb.append(renderStatBlockBreaker("Defense"));
-		sb.append(addField("Natural Armor", curs.getString(48), false));
-		sb.append(addField("AC", curs.getString(12)));
-		sb.append(addField("Hit Dice", curs.getString(47), false));
-		sb.append(addField("HP", curs.getString(13)));
-		sb.append(addField("Fort", curs.getString(14), false));
-		sb.append(addField("Ref", curs.getString(15), false));
-		sb.append(addField("Will", curs.getString(16)));
-		sb.append(addField("Defensive Abilities", curs.getString(17), false));
-		sb.append(addField("DR", curs.getString(18), false));
-		sb.append(addField("Resist", curs.getString(19), false));
-		sb.append(addField("Immune", curs.getString(20), false));
-		sb.append(addField("SR", curs.getString(21), false));
+		sb.append(addField("Natural Armor",
+				CreatureAdapter.CreatureUtils.getNaturalArmor(cursor), false));
+		sb.append(addField("AC", CreatureAdapter.CreatureUtils.getAC(cursor)));
+		sb.append(addField("Hit Dice",
+				CreatureAdapter.CreatureUtils.getHitDice(cursor), false));
+		sb.append(addField("HP", CreatureAdapter.CreatureUtils.getHP(cursor)));
+		sb.append(addField("Fort",
+				CreatureAdapter.CreatureUtils.getFortitude(cursor), false));
+		sb.append(addField("Ref",
+				CreatureAdapter.CreatureUtils.getReflex(cursor), false));
+		sb.append(addField("Will",
+				CreatureAdapter.CreatureUtils.getWill(cursor)));
+		sb.append(addField("Defensive Abilities",
+				CreatureAdapter.CreatureUtils.getDefensiveAbilities(cursor),
+				false));
+		sb.append(addField("DR", CreatureAdapter.CreatureUtils.getDR(cursor),
+				false));
+		sb.append(addField("Resist",
+				CreatureAdapter.CreatureUtils.getResist(cursor), false));
+		sb.append(addField("Immune",
+				CreatureAdapter.CreatureUtils.getImmune(cursor), false));
+		sb.append(addField("SR", CreatureAdapter.CreatureUtils.getSR(cursor),
+				false));
 		sb.append("<br>\n");
-		sb.append(addField("Weakness", curs.getString(22)));
+		sb.append(addField("Weakness",
+				CreatureAdapter.CreatureUtils.getWeaknesses(cursor)));
 		String retval = sb.toString();
 		if (retval.equals(renderStatBlockBreaker("Defense"))) {
 			return "";
@@ -129,19 +139,23 @@ public class CreatureRenderer extends StatBlockRenderer {
 		return retval;
 	}
 
-	private String renderCreatureOffense(Cursor curs) {
+	private String renderCreatureOffense(Cursor cursor) {
 		StringBuffer sb = new StringBuffer();
-		// 23:speed, 24:melee, 25:ranged, 26:space, 27:reach,
-		// 28:special_attacks,
-		// 49:breath_weapon
 		sb.append(renderStatBlockBreaker("Offense"));
-		sb.append(addField("Speed", curs.getString(23)));
-		sb.append(addField("Melee", curs.getString(24)));
-		sb.append(addField("Ranged", curs.getString(25)));
-		sb.append(addField("Space", curs.getString(26)));
-		sb.append(addField("Reach", curs.getString(27)));
-		sb.append(addField("Breath Weapon", curs.getString(49), false));
-		sb.append(addField("Special Attacks", curs.getString(28)));
+		sb.append(addField("Speed",
+				CreatureAdapter.CreatureUtils.getSpeed(cursor)));
+		sb.append(addField("Melee",
+				CreatureAdapter.CreatureUtils.getMelee(cursor)));
+		sb.append(addField("Ranged",
+				CreatureAdapter.CreatureUtils.getRanged(cursor)));
+		sb.append(addField("Space",
+				CreatureAdapter.CreatureUtils.getSpace(cursor)));
+		sb.append(addField("Reach",
+				CreatureAdapter.CreatureUtils.getReach(cursor)));
+		sb.append(addField("Breath Weapon",
+				CreatureAdapter.CreatureUtils.getBreathWeapon(cursor), false));
+		sb.append(addField("Special Attacks",
+				CreatureAdapter.CreatureUtils.getSpecialAttacks(cursor)));
 		String retval = sb.toString();
 		if (retval.equals(renderStatBlockBreaker("Offense"))) {
 			return "";
@@ -150,50 +164,65 @@ public class CreatureRenderer extends StatBlockRenderer {
 	}
 
 	private String renderCreatureSpells(Integer sectionId) {
-		Cursor curs = bookDbAdapter.getCreatureAdapter().getCreatureSpells(
+		Cursor cursor = bookDbAdapter.getCreatureAdapter().getCreatureSpells(
 				sectionId);
 		// 0:name, 1:body
 		try {
 			StringBuffer sb = new StringBuffer();
-			boolean has_next = curs.moveToFirst();
+			boolean has_next = cursor.moveToFirst();
 			while (has_next) {
-				String name = capitalizeString(curs.getString(0));
-				sb.append(addField(name, curs.getString(1)));
-				has_next = curs.moveToNext();
+				String name = capitalizeString(CreatureAdapter.CreatureSpellsUtils
+						.getName(cursor));
+				sb.append(addField(name,
+						CreatureAdapter.CreatureSpellsUtils.getBody(cursor)));
+				has_next = cursor.moveToNext();
 			}
 			return sb.toString();
 		} finally {
-			curs.close();
+			cursor.close();
 		}
 	}
 
-	private String renderCreatureStatistics(Cursor curs) {
+	private String renderCreatureStatistics(Cursor cursor) {
 		StringBuffer sb = new StringBuffer();
-		// 29:strength, 30:dexterity, 31:constitution, 32:intelligence,
-		// 33:wisdom, 34:charisma,
-		// 35:base_attack, 36:cmb, 37:cmd, 38:feats, 39:skills,
-		// 40:racial_modifiers, 41:languages, 42:special_qualities,
-		// 43:gear, 44:combat_gear, 45:other_gear, 46:boon,
 		sb.append(renderStatBlockBreaker("Statistics"));
-		sb.append(addField("Str", curs.getString(29), false));
-		sb.append(addField("Dex", curs.getString(30), false));
-		sb.append(addField("Con", curs.getString(31), false));
-		sb.append(addField("Int", curs.getString(32), false));
-		sb.append(addField("Wis", curs.getString(33), false));
-		sb.append(addField("Cha", curs.getString(34)));
-		sb.append(addField("Base Atk", curs.getString(35), false));
-		sb.append(addField("CMB", curs.getString(36), false));
-		sb.append(addField("CMD", curs.getString(37)));
-		sb.append(addField("Feats", curs.getString(38)));
-		boolean rmExists = curs.getString(40) == null;
-		sb.append(addField("Skills", curs.getString(39), rmExists));
-		sb.append(addField("Racial Modifiers", curs.getString(40)));
-		sb.append(addField("Languages", curs.getString(41)));
-		sb.append(addField("Special Qualities", curs.getString(42)));
-		sb.append(addField("Gear", curs.getString(43)));
-		sb.append(addField("Combat Gear", curs.getString(44)));
-		sb.append(addField("Other Gear", curs.getString(45)));
-		sb.append(addField("Boon", curs.getString(46)));
+		sb.append(addField("Str",
+				CreatureAdapter.CreatureUtils.getStrength(cursor), false));
+		sb.append(addField("Dex",
+				CreatureAdapter.CreatureUtils.getDexterity(cursor), false));
+		sb.append(addField("Con",
+				CreatureAdapter.CreatureUtils.getConstitution(cursor), false));
+		sb.append(addField("Int",
+				CreatureAdapter.CreatureUtils.getIntelligence(cursor), false));
+		sb.append(addField("Wis",
+				CreatureAdapter.CreatureUtils.getWisdom(cursor), false));
+		sb.append(addField("Cha",
+				CreatureAdapter.CreatureUtils.getCharisma(cursor)));
+		sb.append(addField("Base Atk",
+				CreatureAdapter.CreatureUtils.getBaseAttack(cursor), false));
+		sb.append(addField("CMB", CreatureAdapter.CreatureUtils.getCMB(cursor),
+				false));
+		sb.append(addField("CMD", CreatureAdapter.CreatureUtils.getCMD(cursor)));
+		sb.append(addField("Feats",
+				CreatureAdapter.CreatureUtils.getFeats(cursor)));
+		boolean rmExists = CreatureAdapter.CreatureUtils
+				.getRacialModifiers(cursor) == null;
+		sb.append(addField("Skills",
+				CreatureAdapter.CreatureUtils.getSkills(cursor), rmExists));
+		sb.append(addField("Racial Modifiers",
+				CreatureAdapter.CreatureUtils.getRacialModifiers(cursor)));
+		sb.append(addField("Languages",
+				CreatureAdapter.CreatureUtils.getLanguages(cursor)));
+		sb.append(addField("Special Qualities",
+				CreatureAdapter.CreatureUtils.getSpecialQualities(cursor)));
+		sb.append(addField("Gear",
+				CreatureAdapter.CreatureUtils.getGear(cursor)));
+		sb.append(addField("Combat Gear",
+				CreatureAdapter.CreatureUtils.getCombatGear(cursor)));
+		sb.append(addField("Other Gear",
+				CreatureAdapter.CreatureUtils.getOtherGear(cursor)));
+		sb.append(addField("Boon",
+				CreatureAdapter.CreatureUtils.getBoon(cursor)));
 		String retval = sb.toString();
 		if (retval.equals(renderStatBlockBreaker("Statistics"))) {
 			return "";
@@ -201,13 +230,16 @@ public class CreatureRenderer extends StatBlockRenderer {
 		return retval;
 	}
 
-	private String renderCreatureEcology(Cursor curs) {
+	private String renderCreatureEcology(Cursor cursor) {
 		StringBuffer sb = new StringBuffer();
 		// 47:environment, 48:organization, 49:treasure
 		sb.append(renderStatBlockBreaker("Ecology"));
-		sb.append(addField("Environment", curs.getString(47)));
-		sb.append(addField("Organization", curs.getString(48)));
-		sb.append(addField("Treasure", curs.getString(49)));
+		sb.append(addField("Environment",
+				CreatureAdapter.CreatureUtils.getEnvironment(cursor)));
+		sb.append(addField("Organization",
+				CreatureAdapter.CreatureUtils.getOrganization(cursor)));
+		sb.append(addField("Treasure",
+				CreatureAdapter.CreatureUtils.getTreasure(cursor)));
 		String retval = sb.toString();
 		if (retval.equals(renderStatBlockBreaker("Ecology"))) {
 			return "";
